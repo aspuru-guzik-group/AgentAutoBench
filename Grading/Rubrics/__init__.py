251d5500fc6041b46f59e819fe3e4a511b74e94e
Hanyong Xu <140997946+sce0w1c@users.noreply.github.com>
2025-11-04 14:20:34 -0500

    Create __init__.py

---
+++ Grading/Rubrics/__init__.py
@@ -0,0 +1,9 @@
+# Grading/Rubrics/__init__.py
+
+from .TDDFT import RUBRIC_TDDFT as TDDFT_RUBRIC
+from .RingStrain import RUBRIC_RINGSTRAIN as RINGSTRAIN_RUBRIC
+
+__all__ = [
+    "TDDFT_RUBRIC",
+    "RINGSTRAIN_RUBRIC"
+]
