cd518277ce43e1da79e55c5b1f17150967a5d8cc
Hanyong Xu <140997946+sce0w1c@users.noreply.github.com>
2025-12-15 17:08:32 -0500

    Update __init__.py

--- Grading/Rubrics/__init__.py
+++ Grading/Rubrics/__init__.py
@@ -1,9 +1,13 @@
 # Grading/Rubrics/__init__.py
 
+from .pKa import RUBRIC_PKA as PKA_RUBRIC
 from .TDDFT import RUBRIC_TDDFT as TDDFT_RUBRIC
 from .RingStrain import RUBRIC_RINGSTRAIN as RINGSTRAIN_RUBRIC
+from .Fukui import RUBRIC_FUKUI as FUKUI_RUBRIC
 
 __all__ = [
+    "PKA_RUBRIC",
     "TDDFT_RUBRIC",
-    "RINGSTRAIN_RUBRIC"
+    "RINGSTRAIN_RUBRIC",
+    "FUKUI_RUBRIC"
 ]
