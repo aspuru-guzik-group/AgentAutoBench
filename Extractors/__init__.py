20ba8d732b28cc2bf9c3ab5b91ae0f9bbc44661d
Hanyong Xu <140997946+sce0w1c@users.noreply.github.com>
2025-12-15 17:06:20 -0500

    Update __init__.py

--- Extractors/__init__.py
+++ Extractors/__init__.py
@@ -1,3 +1,5 @@
+# Auto_benchmark/Extractors/__init__.py
+
 from .TDDFT import (
     extract_tddft_core,
     extract_tddft_from_md,
@@ -7,12 +9,21 @@ from .pKa import (
     extract_pka_orca_core,
     extract_pka_orca_core_from_folder,
     extract_pka_from_md,
-    extract_pka_core,  # alias to *_orca_core
+    extract_pka_orca_core,  # alias to *_orca_core
 )
 
 from .RingStrain import (
     extract_rs_core,
     RS_extractor_from_md,
+)
+
+from .Solido_Q1 import (
+    extract_solido_q1_core
+)
+
+from .Fukui import (
+    extract_fukui_charges,
+    calculate_fukui_indices
 )
 
 __all__ = [
@@ -29,4 +40,11 @@ __all__ = [
     # RS
     "extract_rs_core",
     "RS_extractor_from_md",
+
+    # Solido_Q1
+    "extract_solido_q1_core",
+
+    # Fukui
+    "extract_fukui_charges",
+    "calculate_fukui_indices",
 ]
